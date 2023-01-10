02620a6db71d3fba4bc439b5aab029498fa3fc4e
AngelikaMann <angelika0769@gmail.com>
2023-01-10 02:57:10 +0100

    Add project files.

---
+++ WSportsStore/Models/Cart.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSportsStore.Models
+{
+    public class Cart
+    {
+        public List<CartLine> Lines { get; set; } = new List<CartLine>();
+        public virtual void AddItem(Product product, int quantity)
+        {
+            CartLine line = Lines
+            .Where(p => p.Product.ProductID == product.ProductID)
+            .FirstOrDefault();
+            if (line == null)
+            {
+                Lines.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        //++++++++++++++++
+        public virtual void RemoveLine(Product product)
+        {
+            CartLine line = Lines
+                        .Where(p => p.Product.ProductID == product.ProductID)
+                        .FirstOrDefault();
+            if (line.Quantity != 1)
+            {
+                line.Quantity = line.Quantity - 1;
+            }
+            else
+            {
+                Lines.RemoveAll(l => l.Product.ProductID == product.ProductID);
+            }
+        } 
+
+
+
+
+
+        //Lines.RemoveAll(l => l.Product.ProductID == product.ProductID);
+        public  virtual decimal ComputeTotalValue() =>
+        Lines.Sum(e => e.Product.Price * e.Quantity);
+        public virtual void Clear() => Lines.Clear();
+    }
+    public class CartLine
+    {
+        public int CartLineID { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ WSportsStore/Pages/Cart.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WSportsStore.Models;
+using WSportsStore.Infrastructure;
+
+namespace WSportsStore.Pages
+{
+    public class CartModel : PageModel
+    {
+        private IStoreRepository repository;
+        public CartModel(IStoreRepository repo, Cart cartSerice)
+        {
+            repository = repo;
+            Cart = cartSerice;
+        }
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+            //Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        }
+        public IActionResult OnPost(long productId, string returnUrl)
+        {
+            Product product = repository.Products
+            .FirstOrDefault(p => p.ProductID == productId);                                                                                                                                                                                                                                                                                                                                                                 
+            //Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            Cart.AddItem(product, 1);
+            //HttpContext.Session.SetJson("cart", Cart);
+            return RedirectToPage(new { returnUrl = returnUrl });
+        }
+        public IActionResult OnPostRemove(long productId, string returnUrl)
+        {
+            Cart.RemoveLine(Cart.Lines.First(cl =>
+            cl.Product.ProductID == productId).Product);
+            return RedirectToPage(new { returnUrl = returnUrl });
+        }
+    }
+}
